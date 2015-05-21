d6941798d40669c118727b7ede214d0aa78b43b8
tostyle <unplugged2d@gmail.com>
2015-05-22 00:02:09 +0700

    newsen mailk

--- index.php
+++ index.php
@@ -1789,43 +1789,27 @@ session_start();
     }
     function sendMail()
     {
-       ini_set('display_errors', 1);
-       error_reporting(E_ALL);
-        $mail = new PHPMailer;
-        // exit;
-        $mail->isSMTP();                   
-                           // Set mailer to use SMTP
-        $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-         $mail->SMTPDebug = 2;
-        $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = 'unplugged2d@gmail.com';                 // SMTP username
-        $mail->Password = 'halflink';                           // SMTP password
-        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-        $mail->Port = 465;                                    // TCP port to connect to
-
-        $mail->From = 'unplugged2d@gmail.com';
-        $mail->FromName = 'Mailer';
-        $mail->addAddress('unplugged2d@gmail.com', 'Joe User');     // Add a recipient
-        // $mail->addReplyTo('info@example.com', 'Information');
-        // $mail->addCC('cc@example.com');
-        // $mail->addBCC('bcc@example.com');
-
-        // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-        $mail->isHTML(true);                                  // Set email format to HTML
-
-        $mail->Subject = 'Here is the subject';
-        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-        
-        // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-//         echo '<pre>';
-// var_dump($mail);exit;
-        if(!$mail->send()) {
-            echo 'Message could not be sent.';
-            echo 'Mailer Error: ' . $mail->ErrorInfo;
-        } else {
-            echo 'Message has been sent';
-        }
-            var_dump($mail);
+       $mail = new PHPMailer(); // create a new object
+        $mail->IsSMTP(); // enable SMTP
+        $mail->SMTPDebug = 2; // debugging: 1 = errors and messages, 2 = messages only
+        $mail->SMTPAuth = true; // authentication enabled
+        $mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for GMail
+        $mail->Host = "smtp.gmail.com";
+        $mail->Port = 465; // or 587
+        $mail->IsHTML(true);
+        $mail->Username = "unplugged2d@gmail.com";
+        $mail->Password = "halflink";
+        $mail->SetFrom("unplugged2d@gmail.com");
+        $mail->Subject = "Test";
+        $mail->Body = "hello";
+        $mail->AddAddress("unplugged2d@gmail.com");
+         if(!$mail->Send())
+            {
+            echo "Mailer Error: " . $mail->ErrorInfo;
+            }
+            else
+            {
+            echo "Message has been sent";
+            }
     }
 
