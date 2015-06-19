4db26efcc3fa4e16ab2a45a68a696676aac23217
tostyle <unplugged2d@gmail.com>
2015-06-19 07:55:39 +0700

    add date

--- templates/register/register.php
+++ templates/register/register.php
@@ -3,13 +3,24 @@
   <div class="col-md-12">
       <?php foreach ($this->data->courseType as $courseType => $typeName): ?>
         <h2>สมัครเรียน<?=$typeName?></h2>
-        <?php if( !empty($this->data->course[$courseType])  ):?>
-        <?php  foreach ($this->data->course[$courseType] as $index => $courseLive): ?>
+        <?php if( !empty($this->data->course[$courseType])  ):
+
+        ?>
+        <?php  foreach ($this->data->course[$courseType] as $index => $courseLive): 
+    
+ $startDateCourse=date_create( $courseLive['start_date'] );
+
+ date_add($startDateCourse,date_interval_create_from_date_string("15 days"));
+
+         $endRegister = date_format($startDateCourse,"Y-m-d");
+        ?>
           <div class="panel panel-default">
             <div class="panel-heading">
               <h3 class="panel-title">
                 <a href="registerDetail?id=<?=$index?>"><?=$courseLive['course_name']?></a> 
                 Level.<?=$courseLive['level']?>
+                <?php //if($courseLive['remain_seat'] ==0 &&):?>
+
               </h3>
             </div>
             <div class="panel-body">
